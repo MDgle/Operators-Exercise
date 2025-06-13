bd689c6d3c2482a44d60e43d56e20ba02830eedf
Matthew Yglesias <mdywrk@gmail.com>
2025-06-12 20:11:33 -0400

    completed excercise 1

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -2,9 +2,21 @@
 {
     public class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            int x = 2;
+            int y = 5;
+            int addition = x + y;
+            int subtraction = x - y;
+            int multiplication = x * y;
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
         }
     }
 }
